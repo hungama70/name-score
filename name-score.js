7ce927b4ad010b88224676b4e623c26aa75a44fb
hungama70 <34588693+hungama70@users.noreply.github.com>
2022-06-19 10:10:52 -0500

    edited

--- name-score.js
+++ name-score.js
@@ -40,26 +40,24 @@ const ALPHA = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
 const nameScore = function(filename) {
   const fileData = fs.readFileSync(filename, 'utf8');
   const nameArray = fileData.replace(/"/g, '').split(',').sort();
-  console.log(fileData)
-  console.log(nameArray)
 
-
-  nameArray.forEach(firstName=>{
+  let ans = 0
+  nameArray.forEach((firstName, i) => {
     let nameSum = 0
+    const namePos = i + 1
     firstName.split('').forEach(letter => {
-        const letterPosition = ALPHA.indexOf(letter) + 1
-        nameSum += letterPosition
+      const letterPosition = ALPHA.indexOf(letter) + 1
+      nameSum += letterPosition
     })
-    console.log(firstName, nameSum)
-
-    
+    const nameVal = nameSum * namePos
+    ans += nameVal
   })
 
-  
-  return 5
+  return ans
 }
 
 const score = nameScore('names.txt')
 
 console.log("Score is", score)
+
 
